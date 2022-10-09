d94a3c75891dd2dcbc1990c394cb78b29efc7700
marcusaltman <84755406+marcusaltman@users.noreply.github.com>
2022-10-08 17:05:19 -0700

    Update Movie.java

--- project1/Movie.java
+++ project1/Movie.java
@@ -2,24 +2,27 @@ package project1;
 
 import java.util.Hashtable;
 
+/**
+ * This class creates objects that represent movies. Each object contains a Hashtable of all of the characters in the respective movie.
+ * @author marcusaltman
+ */
 public class Movie {
 	private String title;
 	private Hashtable<String, String> characters = new Hashtable<>();
 	
-	public Movie(String movieTitle) { // Used in ActorDatabase
+	public Movie(String movieTitle) {
 		title = movieTitle;
 	}
 	
-	public String getTitle() { // Used in Actor
+	public String getTitle() {
 		return title;
 	}
 
-	public void addCharacter(String actorName, String characterName) { // Used in ActorDatabase
+	public void addCharacter(String actorName, String characterName) {
 		characters.put(actorName, characterName);
 	}
 	
-	public String getCharacter(String actorName) { // Used in Actor
+	public String getCharacter(String actorName) {
 		return characters.get(actorName);
 	}
-
 }
