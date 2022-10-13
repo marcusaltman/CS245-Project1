803c4b73240a29837fd5e5ba8ffad64b02f7acce
marcusaltman <84755406+marcusaltman@users.noreply.github.com>
2022-10-12 20:58:09 -0700

    Delete project1 directory

--- project1/Project1.java
+++
@@ -1,9 +0,0 @@
-package project1;
-
-public class Project1 {
-
-	public static void main(String[] args) {
-		ActorDatabase database = new ActorDatabase(args[0]);
-		MovieWall.start(database);
-	}
-}
